cbb4b6b6670a7b33834a70be1e5998c3be836b11
helloayan14 <ayansharma2006@gmail.com>
2025-06-18 21:44:30 +0530

    deploy

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,44 +1,51 @@
+
 const config = {
-  plugins: ["@tailwindcss/postcss"],
-};
+  plugins: ["@tailwindcss/postcss",],
+  
+ 
+};                                                                                                                                                                                                          
+
+
 
 export default config;
 
+// module.exports = {
+//   content: [
+//     './pages/**/*.{js,ts,jsx,tsx,mdx}',
+//     './components/**/*.{js,ts,jsx,tsx,mdx}',
+//     './app/**/*.{js,ts,jsx,tsx,mdx}',
+//   ],
+//   theme: {
+//     extend: {
+//       animation: {
+//         'spin-slow': 'spin 8s linear infinite',
+//       },
+//       backdropBlur: {
+//         xs: '2px',
+//       }
+//     },
+//   },
 
-module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      animation: {
-        'spin-slow': 'spin 8s linear infinite',
-      },
-      backdropBlur: {
-        xs: '2px',
-      }
-    },
-  },
+//   theme: {
+//   extend: {
+//     blur: {
+//       '3xl': '100px',
+//     },
+//     colors: {
+//       neon: '#0fffc1',
+//     },
+//   },
+// },
+// theme: {
+//   extend: {
+//     backgroundImage: {
+//       'grid-dark': "radial-gradient(rgba(255,255,255,0.05) 1px, transparent 1px)",
+//     },
+//   },
+// },
 
-  theme: {
-  extend: {
-    blur: {
-      '3xl': '100px',
-    },
-    colors: {
-      neon: '#0fffc1',
-    },
-  },
-},
-theme: {
-  extend: {
-    backgroundImage: {
-      'grid-dark': "radial-gradient(rgba(255,255,255,0.05) 1px, transparent 1px)",
-    },
-  },
-},
-
-  plugins: [],
-}
+//   plugins: {
+//     tailwindcss:{},
+//     autoprefixer:{}
+//   },
+// }
